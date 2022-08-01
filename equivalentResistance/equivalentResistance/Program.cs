deb5312ef35711b3d5a3bcc1d2d5f8ce4a2f3144
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 15:35:32 +0300

    equivalent resistances
    internship

---
+++ equivalentResistance/equivalentResistance/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r1, r2, r3, rt;
+            int position;
+
+            Console.WriteLine("Enter the value of R1 resistance: ");
+            r1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of R2 resistance: ");
+            r2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of R3 resistance: ");
+            r3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("1. Series resistance");
+            Console.WriteLine("2. Parallel resistance");
+            Console.Write("Please choose the position of resistances...");
+
+            position = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("**************************************************************");
+            if (position == 1)
+            {
+                rt = r1 + r2 + r3;
+            }
+            else if (position == 2)
+            {
+                rt = 1 / ((1 / r1) + (1 / r2) + (1 / r3));
+            }
+            else
+            {
+                rt = -1;
+                Console.WriteLine("You chose a wrong transaction...");
+            }
+            Console.WriteLine("Equivalent resistance: " + rt + " Ohm");
+            Console.ReadLine();
+        }
+
+    }
+
+}
